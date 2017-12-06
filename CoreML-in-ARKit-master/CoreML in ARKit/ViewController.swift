45b6198da81462dd71e5639bbbeb7807ef42107b
sebastiencrettaz <sebastien.crettaz@edu.hefr.ch>
2017-12-06 10:58:36 +0100

    Add PaperjamRemover v0.9
    OK but some errors, application crash sometimes...

--- CoreML-in-ARKit-master/CoreML in ARKit/ViewController.swift
+++ CoreML-in-ARKit-master/CoreML in ARKit/ViewController.swift
@@ -16,15 +16,14 @@ class ViewController: UIViewController, ARSKViewDelegate {
 
     // SCENE
     @IBOutlet var sceneView: ARSKView!
-    let bubbleDepth : Float = 0.01 // the 'depth' of 3D text
     var latestPrediction : String = "…" // a variable containing the latest CoreML prediction
+    var objectName:String = "…" // variable containing the value of the current object found by CoreML
+    var prediction:String = "…" // variable containing the level of prediction
     
     // COREML
     var visionRequests = [VNRequest]()
     let dispatchQueueML = DispatchQueue(label: "com.hw.dispatchqueueml") // A Serial Queue
     @IBOutlet weak var debugTextView: UITextView!
-    
-    var cameraTransform = matrix_float4x4()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,12 +40,6 @@ class ViewController: UIViewController, ARSKViewDelegate {
         
         // Set the scene to the view
         sceneView.presentScene(scene)
-        
-        //////////////////////////////////////////////////
-        // Tap Gesture Recognizer
-        //let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(gestureRecognize:)))
-        //view.addGestureRecognizer(tapGesture)
-        //////////////////////////////////////////////////
         
         // Set up Vision Model
         guard let selectedModel = try? VNCoreMLModel(for: MyModel().model) else {
@@ -68,10 +61,10 @@ class ViewController: UIViewController, ARSKViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
         // Enable plane detection
-        configuration.planeDetection = .horizontal
+        //configuration.planeDetection = .horizontal
         
         // Run the view's session
-        sceneView.session.run(configuration)
+        self.sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -85,70 +78,25 @@ class ViewController: UIViewController, ARSKViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
-    // MARK: - ARSCNViewDelegate
-    
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        DispatchQueue.main.async {
-            // Do any desired updates to SceneKit here.
-        }
-    }
     
     // MARK: - Status Bar: Hide
     override var prefersStatusBarHidden : Bool {
         return true
     }
     
-    // MARK: - Interaction
-    
-//    @objc func handleTap(gestureRecognize: UITapGestureRecognizer) {
-//        // HIT TEST : REAL WORLD
-//        // Get Screen Centre
-//        let screenCentre : CGPoint = CGPoint(x: self.sceneView.bounds.midX, y: self.sceneView.bounds.midY)
-//
-//        let arHitTestResults : [ARHitTestResult] = sceneView.hitTest(screenCentre, types: [.featurePoint]) // Alternatively, we could use '.existingPlaneUsingExtent' for more grounded hit-test-points.
-//        if let closestResult = arHitTestResults.first {
-//            // Get Coordinates of HitTest
-//           // let transform : matrix_float4x4 = closestResult.worldTransform
-//           // let worldCoord : SCNVector3 = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
-//
-//            // Create a transform with a translation of 3 meters in front of the camera
-//            var translation = matrix_identity_float4x4
-//            translation.columns.3.z = -0.5
-//            let transform = simd_mul(closestResult.worldTransform, translation)
-//
-//            // Add a new anchor to the session
-//            let anchor = ARAnchor(transform: transform)
-//
-//            // Create 3D Text
-//            //let node : SKNode = createNewBubbleParentNode(latestPrediction)
-//
-//            sceneView.session.add(anchor: anchor)
-//            //node.position = worldCoord
-//        }
-//    }
-    // This function is called when an ARAnchor is added
-    func view(_ view: ARSKView,nodeFor anchor: ARAnchor) -> SKNode? {
-        let labelNode = SKLabelNode(text: "👋🏻")
-        labelNode.horizontalAlignmentMode = .center
-        labelNode.verticalAlignmentMode = .center
-        //labelNode.fontSize = 1
-        return labelNode;
-    }
-    
     // MARK: - CoreML Vision Handling
-    
     func loopCoreMLUpdate() {
         // Continuously run CoreML whenever it's ready. (Preventing 'hiccups' in Frame Rate)
-        
         dispatchQueueML.async {
             // 1. Run Update.
             self.updateCoreML()
             
-            // 2. Loop this function.
+            // 2. Add nodes in the view
+            self.addObject()
+            
+            // 3. Loop this function.
             self.loopCoreMLUpdate()
         }
-        
     }
     
     func classificationCompleteHandler(request: VNRequest, error: Error?) {
@@ -180,36 +128,53 @@ class ViewController: UIViewController, ARSKViewDelegate {
             self.debugTextView.text = debugText
             
             // Store the latest prediction
-            var objectName:String = "…"
-            var prediction:String = "…"
-            prediction = classifications.components(separatedBy: "- ")[1]
-            objectName = classifications.components(separatedBy: "-")[0]
-            objectName = objectName.components(separatedBy: ",")[0]
+
+            self.prediction = classifications.components(separatedBy: "- ")[1]
+            self.objectName = classifications.components(separatedBy: "-")[0]
+            self.objectName = self.objectName.components(separatedBy: ",")[0]
+        }
+    }
+    
+    func addObject(){
+        let predictionFloat = (self.prediction as NSString).doubleValue
+        
+        if(predictionFloat > 0.5){
+            guard let sceneView = self.sceneView else {
+                return
+            }
+            // Variable used to find position of the object
+            //var position = CGPoint(x:0,y:0)
             
-            let predictionFloat = (prediction as NSString).doubleValue
+            //position.x = self.sceneView.bounds.midX
+            //position.y = self.sceneView.bounds.midY
             
-            if(predictionFloat > 0.5){
-                // Variable used to find position of the object
-                var position = CGPoint(x:0,y:0)
-                
-                position.x = self.sceneView.bounds.midX
-                position.y = self.sceneView.bounds.midY
-                
-                // Create anchor using the camera's current position
+            if let currentFrame = sceneView.session.currentFrame {
                 // Create a transform with a translation of 3 meters in front of the camera
                 var translation = matrix_identity_float4x4
-                translation.columns.3.z = -10
-                let transform = simd_mul(self.cameraTransform, translation)
+                translation.columns.3.z = -0.2
+                let transform = simd_mul(currentFrame.camera.transform, translation)
                 print(transform)
+                
                 // Add a new anchor to the session
                 let anchor = ARAnchor(transform: transform)
-                self.sceneView.session.add(anchor: anchor)
-                    
-                // Update last object found
-                self.latestPrediction = objectName
-                
+                //print(anchor)
+                sceneView.session.add(anchor: anchor)
             }
+            // Update last object found
+            self.latestPrediction = self.objectName
         }
+    }
+    
+    // This function is called when an ARAnchor is added
+   func view(_ view: ARSKView,nodeFor anchor: ARAnchor) -> SKNode? {
+       let labelNode = SKLabelNode(text: self.latestPrediction)
+       labelNode.horizontalAlignmentMode = .center
+       labelNode.verticalAlignmentMode = .center
+        //labelNode.xScale = 0.05
+        //labelNode.yScale = 0.05
+        //print(view)
+        //labelNode.fontSize = 1
+        return labelNode;
     }
     
     func updateCoreML() {
@@ -218,7 +183,6 @@ class ViewController: UIViewController, ARSKViewDelegate {
         let pixbuff : CVPixelBuffer? = (sceneView.session.currentFrame?.capturedImage)
         if pixbuff == nil { return }
         let ciImage = CIImage(cvPixelBuffer: pixbuff!)
-        cameraTransform = (sceneView.session.currentFrame?.camera.transform)!
         // Note: Not entirely sure if the ciImage is being interpreted as RGB, but for now it works with the Inception model.
         // Note2: Also uncertain if the pixelBuffer should be rotated before handing off to Vision (VNImageRequestHandler) - regardless, for now, it still works well with the Inception model.
         
---
+++ PaperjamRemove/PaperjamRemove/Scene.swift
@@ -0,0 +1,88 @@
+//
+//  Scene.swift
+//  PaperjamRemove
+//
+//  Created by Lin on 04.12.17.
+//  Copyright © 2017 Human Tech. All rights reserved.
+//
+
+import SpriteKit
+import ARKit
+import Vision
+
+class Scene: SKScene {
+    var latestPrediction : String = "…" // a variable containing the latest CoreML prediction
+    static var objectName:String = "…" // variable containing the value of the current object found by CoreML
+    var prediction:String = "…" // variable containing the level of prediction
+    
+    override func didMove(to view: SKView) {
+        // Setup your scene here
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        guard let sceneView = self.view as? ARSKView else {
+            return
+        }
+        
+        var predictionFloat : Double = 0.0
+        // Variable used to find position of the object
+        //var position = CGPoint(x:0,y:0)
+        
+        //position.x = self.sceneView.bounds.midX
+        //position.y = self.sceneView.bounds.midY
+        
+        if let currentFrame = sceneView.session.currentFrame {
+            DispatchQueue.global(qos: .background).async {
+                do {
+                    let model = try VNCoreMLModel(for:MyModel().model)
+                    let request = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
+                        // Jump onto the main thread
+                        DispatchQueue.main.async {
+                            // Access the first result in the array after casting the array as a VNClassificationObservation array
+                            guard let results = request.results as? [VNClassificationObservation] else {
+                                print ("No results?")
+                                return
+                            }
+                            // Get Classifications
+                            let classifications = results[0...1] // top 2 results
+                                .flatMap({ $0 })
+                                .map({ "\($0.identifier) \(String(format:"- %.2f", $0.confidence))" })
+                                .joined(separator: "\n")
+                            
+                            // Store the latest prediction
+                            self.prediction = classifications.components(separatedBy: "- ")[1]
+                            Scene.objectName = classifications.components(separatedBy: "-")[0]
+                            Scene.objectName = Scene.objectName.components(separatedBy: ",")[0]
+                            
+                            //print(result.identifier)
+                        }
+                    })
+                    
+                    let handler = VNImageRequestHandler(cvPixelBuffer: currentFrame.capturedImage, options: [:])
+                    try handler.perform([request])
+                } catch {}
+                
+                predictionFloat = (self.prediction as NSString).doubleValue
+                print(self.prediction)
+                
+                if(predictionFloat > 0.5 && self.latestPrediction != Scene.objectName){
+                    // Create a transform with a translation of 0.2 meters in front of the camera
+                    var translation = matrix_identity_float4x4
+                    translation.columns.3.z = -0.4
+                    let transform = simd_mul(currentFrame.camera.transform, translation)
+                    print(translation)
+                    
+                    // Add a new anchor to the session
+                    let anchor = ARAnchor(transform: transform)
+                    
+                    // Set the identifier
+                    //ARBridge.shared.anchorsToIdentifiers[anchor] = result.identifier
+                    sceneView.session.add(anchor: anchor)
+                    
+                    // Update last object found
+                    self.latestPrediction = Scene.objectName
+                }
+            }
+        }
+    }
+}
